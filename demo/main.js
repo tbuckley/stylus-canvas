b14cc88fe0615a14dbf27ee18de7a8f82244a503
Tom Buckley <tbuckley89@gmail.com>
2018-12-03 01:44:57 +0000

    Added eslint

--- demo/main.js
+++ demo/main.js
@@ -1,82 +1,85 @@
+/* eslint-env browser */
+
 let canvas = null;
 let ctx = null;
-let strokes = [];
+const STROKES = [];
 
 function getPoint(e) {
-    let rect = canvas.getBoundingClientRect();
-    return {
-        x: e.clientX - rect.left,
-        y: e.clientY - rect.top,
-    };
+  const rect = canvas.getBoundingClientRect();
+  return {
+    x: e.clientX - rect.left,
+    y: e.clientY - rect.top,
+  };
 }
-
-async function main() {
-    // Wait for canvas to exist
-    if(!customElements.get("stylus-canvas")) {
-        await customElements.whenDefined("stylus-canvas");
-    }
-
-
-    // Get the canvas
-    canvas = document.querySelector("stylus-canvas");
-    ctx = canvas.getContext("2d", {lowLatency: true});
-
-    let strokes = [];
-    let isDrawing = false;
-
-    // Set up resize/rotate handlers
-    canvas.addEventListener("resize", () => drawAllStrokes(strokes));
-    canvas.addEventListener("rotate", e => {
-        canvas.updateTransform2d(ctx);
-        drawAllStrokes(strokes);
-    });
-
-    canvas.addEventListener("pointerdown", (e) => {
-        const p = getPoint(e);
-        strokes.push([p]);
-        isDrawing = true;
-    });
-    canvas.addEventListener("pointermove", (e) => {
-        if(isDrawing) {
-            const p = getPoint(e);
-            strokes[strokes.length-1].push(p);
-            drawLatestStrokePortion(strokes);
-        }
-    });
-    canvas.addEventListener("pointerup", () => {
-        isDrawing = false;
-    });
-}
-
-main();
 
 function drawLatestStrokePortion(strokes) {
-    ctx.strokeStyle = "#000";
-    
-    let lastStroke = strokes[strokes.length-1];
-    let prev = lastStroke[lastStroke.length-2];
-    let p = lastStroke[lastStroke.length-1];
-    ctx.moveTo(prev.x, prev.y);
-    ctx.lineTo(p.x, p.y);
-    ctx.stroke();
+  ctx.strokeStyle = '#000';
+
+  const lastStroke = strokes[strokes.length - 1];
+  const prev = lastStroke[lastStroke.length - 2];
+  const p = lastStroke[lastStroke.length - 1];
+  ctx.moveTo(prev.x, prev.y);
+  ctx.lineTo(p.x, p.y);
+  ctx.stroke();
 }
 
 function drawAllStrokes(strokes) {
-    ctx.fillStyle = "red";
-    ctx.fillRect(0,0,600,400);
+  ctx.fillStyle = 'red';
+  ctx.fillRect(0, 0, 600, 400);
 
-    // Draw arrow
-    ctx.strokeStyle = "#000";
-    strokes.forEach(stroke => {
-        ctx.beginPath();
-        
-        let prev = stroke[0];
-        for(let i = 1; i < stroke.length; i++) {
-            let p = stroke[i];
-            ctx.moveTo(prev.x, prev.y);
-            ctx.lineTo(p.x, p.y);
-            ctx.stroke();
-            prev = p;
-        }
-    });
+  // Draw arrow
+  ctx.strokeStyle = '#000';
+  strokes.forEach((stroke) => {
+    ctx.beginPath();
+
+    let prev = stroke[0];
+    for (let i = 1; i < stroke.length; i += 1) {
+      const p = stroke[i];
+      ctx.moveTo(prev.x, prev.y);
+      ctx.lineTo(p.x, p.y);
+      ctx.stroke();
+      prev = p;
+    }
+  });
 }
+
+function main() {
+  // Get the canvas
+  canvas = document.querySelector('stylus-canvas');
+  ctx = canvas.getContext('2d', { lowLatency: true });
+
+  let isDrawing = false;
+
+  // Set up resize/rotate handlers
+  canvas.addEventListener('resize', () => drawAllStrokes(STROKES));
+  canvas.addEventListener('rotate', (e) => {
+    canvas.updateTransform2d(ctx);
+    drawAllStrokes(STROKES);
+  });
+
+  canvas.addEventListener('pointerdown', (e) => {
+    const p = getPoint(e);
+    STROKES.push([p]);
+    isDrawing = true;
+  });
+  canvas.addEventListener('pointermove', (e) => {
+    if (isDrawing) {
+      const p = getPoint(e);
+      STROKES[STROKES.length - 1].push(p);
+      drawLatestStrokePortion(STROKES);
+    }
+  });
+  canvas.addEventListener('pointerup', () => {
+    isDrawing = false;
+  });
+}
+
+async function init() {
+  // Wait for canvas to exist
+  if (!customElements.get('stylus-canvas')) {
+    await customElements.whenDefined('stylus-canvas');
+  }
+  main();
+}
+
+init();
--- stylus-canvas.js
+++ stylus-canvas.js
@@ -1,152 +1,156 @@
+/* eslint-env browser */
+
 // ROTATION_EPSILON is the amount to rotate the canvas when
 // disabling low latency. It should be large enough to cause
 // the canvas to drop out of a hw overlay, but not large enough
 // to be noticeable to the user.
 const ROTATION_EPSILON = 0.0001;
 
-let tmpl = document.createElement('template');
+const tmpl = document.createElement('template');
 tmpl.innerHTML = `
   <style>
-    :host {display: block;}
-    canvas {
-        touch-action: none;
-        transform-origin: 50% 50%;
-    }
+  :host {display: block;}
+  canvas {
+    touch-action: none;
+    transform-origin: 50% 50%;
+  }
   </style>
   <canvas></canvas>
 `;
 
-class StylusCanvas extends HTMLElement {
-    constructor() {
-        super();
-
-        // Initialize properties
-        this._lowLatencyEnabled = true;
-        this._rotation = 0;
-        this._dimensions = {width: this.offsetWidth, height: this.offsetHeight};
-
-        // Create shadow root
-        let shadowRoot = this.attachShadow({mode: "open"});
-        shadowRoot.appendChild(tmpl.content.cloneNode(true));
-        this.canvas = shadowRoot.querySelector("canvas");
-
-        // Observe resizes
-        this.resizeObserver = new ResizeObserver(entries => {
-            for(let entry of entries) {
-                const cr = entry.contentRect;
-                const target = entry.target;
-
-                this._dimensions = {width: cr.width, height: cr.height};
-                this._updateCanvasSize();
-                this.dispatchEvent(new CustomEvent("resize", {
-                    width: cr.width,
-                    height: cr.height,
-                }));
-            }
-        });
-        this.resizeObserver.observe(this);
-
-        // Observe orientation changes
-        screen.orientation.onchange = e => {
-            this._handleRotation();
-        };
-        this._handleRotation();
-    }
-
-    _handleRotation() {
-        if(this._rotation === screen.orientation.angle) {
-            return;
-        }
-
-        this._rotation = screen.orientation.angle;
-        this._updateTransform();
-        this._updateCanvasSize();
-        this.dispatchEvent(new CustomEvent("rotate", {detail: {angle: this._rotation}}));
-    }
-    _updateTransform() {
-        console.log("_updateTransform");
-        let rotation = this._rotation;
-        if(!this._lowLatencyEnabled) {
-            // TODO Use more reliable method to disable low latency
-            rotation += ROTATION_EPSILON;
-        }
-
-        let shiftX = 0, shiftY = 0;
-        if(this._rotation == 90 || this._rotation == 270) {
-            let [width, height] = this._getCanvasWidthHeight();
-            let w2 = width/2;
-            let h2 = height/2;
-            shiftX = h2-w2;
-            shiftY = w2-h2;
-        }
-
-        let translateTransform = `translate(${shiftX}px, ${shiftY}px)`;
-        let rotationTransform = `rotate(${rotation}deg)`;
-        this.canvas.style.transform = translateTransform + " " + rotationTransform;
-    }
-    _getCanvasWidthHeight() {
-        let width = this._dimensions.width;
-        let height = this._dimensions.height;
-        if(this._rotation == 0 || this._rotation == 180) {
-            return [width, height];
-        }
-        return [height, width];
-    }
-    _updateCanvasSize() {
-        let [width, height] = this._getCanvasWidthHeight();
-
-        this.canvas.width = width;
-        this.canvas.height = height;
-        this.canvas.style.width = `${width}px`;
-        this.canvas.style.height = `${height}px`;
-    }
-
-    // API
-
-    getContext(contextId, contextAttributes) {
-        contextAttributes = contextAttributes || {};
-
-        // Check that parameters will allow for low-latency
-        if(contextAttributes["lowLatency"] !== true) {
-            throw new Error("getContext(id, attrs) must include {lowLatency: true}");
-        }
-
-        return this.canvas.getContext(contextId, contextAttributes);
-    }
-
-    // setLowLatency(enabled) allows the app to programmatically toggle low 
-    // latency. It may be useful to disable low latency when rendering large
-    // changes, such as when panning. Otherwise tearing artifacts may be 
-    // visible.
-    setLowLatency(enabled) {
-        if(this._lowLatencyEnabled === enabled) {
-            return;
-        }
-
-        this._lowLatencyEnabled = enabled;
-        this._updateTransform();
-    }
-
-    updateTransform2d(ctx) {
-        // Reset transform
-        ctx.setTransform(1, 0, 0, 1, 0, 0);
-        
-        // Move origin based on rotation
-        let [width, height] = this._getCanvasWidthHeight();
-        switch(this._rotation) {
-            case 0: break;
-            case 180: ctx.translate(width,height); break;
-            case 90: ctx.translate(0,height); break;
-            case 270: ctx.translate(width,0); break;
-        }
-
-        // Rotate context
-        const angleRadians = this._rotation * Math.PI / 180;
-        ctx.rotate(-angleRadians);
-    }
+function getCanvasWidthHeight(dimensions, rotation) {
+  const { width, height } = dimensions;
+  if (rotation === 0 || rotation === 180) {
+    return [width, height];
+  }
+  return [height, width];
 }
 
-customElements.define("stylus-canvas", StylusCanvas);
+export default class StylusCanvas extends HTMLElement {
+  constructor() {
+    super();
+
+    // Initialize properties
+    this.lowLatencyEnabled = true;
+    this.rotation = 0;
+    this.dimensions = { width: this.offsetWidth, height: this.offsetHeight };
+
+    // Create shadow root
+    const shadowRoot = this.attachShadow({ mode: 'open' });
+    shadowRoot.appendChild(tmpl.content.cloneNode(true));
+    this.canvas = shadowRoot.querySelector('canvas');
+
+    // Observe resizes
+    this.resizeObserver = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        const cr = entry.contentRect;
+        const target = entry.target;
+
+        this.dimensions = { width: cr.width, height: cr.height };
+        this._updateCanvasSize();
+        this.dispatchEvent(new CustomEvent('resize', {
+          width: cr.width,
+          height: cr.height,
+        }));
+      }
+    });
+    this.resizeObserver.observe(this);
+
+    // Observe orientation changes
+    window.screen.orientation.onchange = () => {
+      this.handleRotation();
+    };
+    this.handleRotation();
+  }
+
+  _handleRotation() {
+    if (this.rotation === window.screen.orientation.angle) {
+      return;
+    }
+
+    this.rotation = window.screen.orientation.angle;
+    this.updateTransform();
+    this.updateCanvasSize();
+    this.dispatchEvent(new CustomEvent('rotate', { detail: { angle: this.rotation } }));
+  }
+
+  updateTransform() {
+    let { rotation } = this;
+    if (!this.lowLatencyEnabled) {
+      // TODO Use more reliable method to disable low latency
+      rotation += ROTATION_EPSILON;
+    }
+
+    let shiftX = 0; let
+      shiftY = 0;
+    if (this.rotation === 90 || this.rotation === 270) {
+      const [width, height] = getCanvasWidthHeight(this.dimensions, this.rotation);
+      const w2 = width / 2;
+      const h2 = height / 2;
+      shiftX = h2 - w2;
+      shiftY = w2 - h2;
+    }
+
+    const translateTransform = `translate(${shiftX}px, ${shiftY}px)`;
+    const rotationTransform = `rotate(${rotation}deg)`;
+    this.canvas.style.transform = `${translateTransform} ${rotationTransform}`;
+  }
+
+  _updateCanvasSize() {
+    const [width, height] = getCanvasWidthHeight(this.dimensions, this.rotation);
+
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.canvas.style.width = `${width}px`;
+    this.canvas.style.height = `${height}px`;
+  }
+
+  // API
+
+  getContext(contextId, optionalContextAttributes) {
+    const contextAttributes = optionalContextAttributes || {};
+
+    // Check that parameters will allow for low-latency
+    if (contextAttributes.lowLatency !== true) {
+      throw new Error('getContext(id, attrs) must include {lowLatency: true}');
+    }
+
+    return this.canvas.getContext(contextId, contextAttributes);
+  }
+
+  // setLowLatency(enabled) allows the app to programmatically toggle low
+  // latency. It may be useful to disable low latency when rendering large
+  // changes, such as when panning. Otherwise tearing artifacts may be
+  // visible.
+  setLowLatency(enabled) {
+    if (this.lowLatencyEnabled === enabled) {
+      return;
+    }
+
+    this.lowLatencyEnabled = enabled;
+    this.updateTransform();
+  }
+
+  updateTransform2d(ctx) {
+    // Reset transform
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+
+    // Move origin based on rotation
+    const [width, height] = getCanvasWidthHeight(this.dimensions, this.rotation);
+    switch (this.rotation) {
+      case 90: ctx.translate(0, height); break;
+      case 180: ctx.translate(width, height); break;
+      case 270: ctx.translate(width, 0); break;
+      default: break;
+    }
+
+    // Rotate context
+    const angleRadians = this.rotation * Math.PI / 180;
+    ctx.rotate(-angleRadians);
+  }
+}
+
+customElements.define('stylus-canvas', StylusCanvas);
 
 // Useful functions
 
@@ -155,9 +159,9 @@ customElements.define("stylus-canvas", StylusCanvas);
 // more and more delayed.
 let fence = null;
 export function glDrawWithBackPressure(ctx, drawFn) {
-    if (fence && ctx.getSyncParameter(fence, ctx.SYNC_STATUS) == ctx.UNSIGNALED) {
-        return;  // Skip a frame to not overload the GL command queue.
-    }
-    drawFn();
-    fence = ctx.fenceSync(ctx.SYNC_GPU_COMMANDS_COMPLETE, 0);  // Insert a fence.
+  if (fence && ctx.getSyncParameter(fence, ctx.SYNC_STATUS) == ctx.UNSIGNALED) {
+    return; // Skip a frame to not overload the GL command queue.
+  }
+  drawFn();
+  fence = ctx.fenceSync(ctx.SYNC_GPU_COMMANDS_COMPLETE, 0); // Insert a fence.
 }
